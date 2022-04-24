579729591f8f620aa2e163ef246ad7277605f6a9
Daniel Charles <daniel.charles@pelco.com>
2022-04-24 04:14:31 +0000

    feat: add ability to make a set of word searches

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import grid_builder
 import pdf_renderer
 
 print('Word Search Generator')
-grid = grid_builder.build()
+grids = grid_builder.build()
 FILE = 'word-search.pdf'
 print(f'Writing grid as {FILE}')
-pdf_renderer.render(grid, FILE)
+pdf_renderer.render(grids, FILE)
