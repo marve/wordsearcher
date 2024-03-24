6c8fb4569e1162ccdc9e4498c6ac88f29ed91e5a
Daniel Charles <pdanielcharles@gmail.com>
2024-03-24 05:11:03 +0000

    feat(pdfRenderer): increase text size and positioning to fill page

--- src/pdf_renderer.py
+++ src/pdf_renderer.py
@@ -19,30 +19,31 @@ def render_file(grids: list[Grid], path: str):
     for grid in grids:
         _add_page(grid, pdf)
     pdf.output(name = path, dest = 'F')
+    pdf.set_margins(0, 0, 0)
 
 def _add_page(grid: Grid, pdf: FPDF):
     pdf.add_page()
     pdf.add_font(FONT_NAME, '', FONT_FILE, uni = True)
+    pdf.set_font(FONT_NAME, '', 24)
+    pdf.cell(w = 0, h = 0, txt = grid.title, align = 'C')
     pdf.set_font(FONT_NAME, '', 20)
-    pdf.cell(w = 0, h = 10, txt = grid.title, align = 'C')
-    pdf.set_font(FONT_NAME, '', 16)
-    top_gap = 31.5
-    left_gap = 35
-    row_height = 7.0
+    top_gap = 17.0
+    left_gap = 5.0
+    row_height = 10.0
     cell_width = row_height
 
     for i, row in enumerate(grid.arry):
         pdf.set_xy(x = left_gap, y = (row_height * i) + top_gap)
         for char in row:
             pdf.cell(w = cell_width, h = row_height, txt = char.upper(), align = 'C')
-    pdf.rect(x = left_gap, y = 30, w = 141, h = 142.5, style = 'D')
+    pdf.rect(x = left_gap, y = 15.5, w = 200, h = 202.5, style = 'D')
 
-    pdf.set_font(FONT_NAME, '', 14)
-    num_cols = 4
-    top_gap = 182.5
-    left_gap = 10
-    row_height = 6
-    cell_width = 50
+    pdf.set_font(FONT_NAME, '', 21)
+    num_cols = 3
+    top_gap = 218.5
+    left_gap = 5
+    row_height = 8.35
+    cell_width = 70
     word_table = [grid.words[i:i+num_cols] for i in range(0,len(grid.words),num_cols)]
     for i, row in enumerate(word_table):
         pdf.set_xy(x = left_gap, y = (row_height * i) + top_gap)
